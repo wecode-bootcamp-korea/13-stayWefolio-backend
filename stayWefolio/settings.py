1754e2432a43041f00c91f8aeeb022f4ba52b90b
jek <jihoon.kim89@gmail.com>
2020-10-21 13:47:40 +0900

    Add: added user model convention, settings.py  - user 모델 컨벤션 수정  - settings.py 앱 추가

--- stayWefolio/settings.py
+++ stayWefolio/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
+    'hotel',
+    'booking',
     #'django.contrib.admin',
     #'django.contrib.auth',
     'django.contrib.contenttypes',
