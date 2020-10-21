00f74eae33a13355766e74b7580c4106eb6eb19c
jek <jihoon.kim89@gmail.com>
2020-10-21 21:58:33 +0900

    Add: added user login/signup view  - 로그인/회원가입 API 구현  - 회원가입 비밀번호 암호화  - 로그인 성공시 토큰 리턴  - 로그인 API는 프론트와 접속 확인. 회원가입 API는 추후 테스트 예정  - url 수정 및 추가  - 불필요한 admin.py의 import 삭제

--- user/views.py
+++ user/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.views import View
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+
+from .models import User
+from my_settings import SECRET_KEY, ALGORITHM
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data=json.loads(request.body)
+            email=data['email']
+            name=data['name']
+            password=data['password']
+            if '@' not in email or '.' not in email:
+                return JsonResponse({'message': 'INVALID EMAIL'}, status=400)
+            if len(password)<8:
+                return JsonResponse({'message': 'PASSWORD TOO SHORT'}, status=400)
+
+            hashed_password=bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_password=hashed_password.decode('utf-8')
+            User(
+                email=email,
+                name=name,
+                password=decoded_password
+            ).save()
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+        except KeyError:
+            check_list=['email', 'name', 'password']
+            for key in check_list:
+                if key not in data.keys():
+                    key=key.upper()
+                    return JsonResponse({'message': f"{key} FIELD IS MISSING"}, status=400)
+
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data=json.loads(request.body)
+            email=data['email']
+            password=data['password']
+            user=get_object_or_404(User, email=email)
+
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                access_token=jwt.encode({'user_id':user.id}, SECRET_KEY, algorithm=ALGORITHM)    
+                decoded_token=access_token.decode('utf-8')
+                return JsonResponse({'TOKEN': decoded_token}, status=200)
+            return JsonResponse({'message': 'WRONG PASSWORD'}) 
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
