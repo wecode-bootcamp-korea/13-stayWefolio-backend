4d0a796f6283ca2cc159b4427b7c76909185890f
jek <jihoon.kim89@gmail.com>
2020-10-21 23:19:45 +0900

    Add: added authorization decorator  - 인가 데코레이터 추가  - utils파일 추가

---
+++ user/utils.py
@@ -0,0 +1,17 @@
+import jwt
+
+from django.http import JsonResponse
+
+from my_settings import SECRET_KEY, ALGORITHM
+
+def authorize_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            token=request.headers.get("Authorization")
+            payload=jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+            request.user=payload['user_id']
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message': 'INVALID TOKEN'}, status=400)
+
+        return func(self, request, *args, **kwargs)
+    return wrapper
