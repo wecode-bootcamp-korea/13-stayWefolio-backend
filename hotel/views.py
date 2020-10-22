62347b7a47654da0cb64669689da685454c17050
jek <jihoon.kim89@gmail.com>
2020-10-22 21:12:26 +0900

    Edit: edited mainpage view  - 리턴하는 딕셔너리의 키값을 convention에 맞게 수정  - index가 필요 없어졌으므로 i 대신 hotel로 보기 쉽게 수정

--- hotel/views.py
+++ hotel/views.py
@@ -8,31 +8,24 @@ from .models          import Hotel
 
 class MainBannerView(View):
     def get(self, request):
-        try:
-            hotels  = Hotel.objects.all()[:10]
-            banners = [{
-                'index'    : i+1,
-                'hotelName': hotels[i].name,
-                'desc'     : hotels[i].introduction,
-                'image'    : hotels[i].thumbnail_url,
-            } for i in range(len(hotels))]
-            return JsonResponse({'data': banners}, status=200)
-        except ValueError as e:
-            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
+        hotels  = Hotel.objects.all()[:10]
+        banners = [{
+            'hotel_name': hotel.name,
+            'desc'      : hotel.introduction,
+            'image'     : hotel.thumbnail_url,
+        } for hotel in hotels]
+        return JsonResponse({'data': banners}, status=200)
 
 class MagazineView(View):
     def get(self, request):
-        try:
-            hotels    = Hotel.objects.all()[:2]
-            magazines = [{
-                'index'    : i+1,
-                'hotelName': hotels[i].name,
-                'type'     : hotels[i].category.name,
-                'location' : hotels[i].location.name,
-                'descTitle': hotels[i].description_title,
-                'desc'     : hotels[i].description_first,
-                'image'    : hotels[i].thumbnail_url,
-            }  for i in range(len(hotels))]
-            return JsonResponse({'data': magazines}, status=200)
-        except ValueError as e:
-            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
+        hotels    = Hotel.objects.all()[:2]
+        magazines = [{
+            'hotel_name': hotel.name,
+            'type'      : hotel.category.name,
+            'location'  : hotel.location.name,
+            'desc_title': hotel.description_title,
+            'desc'      : hotel.description_first,
+            'image'     : hotel.thumbnail_url,
+        }  for hotel in hotels]
+        return JsonResponse({'data': magazines}, status=200)
+
