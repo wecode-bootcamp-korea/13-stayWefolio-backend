e9558d9a87ad294ce4f5f2da97e6b5ff251b9025
jek <jihoon.kim89@gmail.com>
2020-10-22 22:36:19 +0900

    Add: added picksview  - Picks페이지를 뿌려주는 API 구현  - API url추가  - aggregate 사용해서 호텔의 방들 중 최대요금, 최저요금 계산

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views      import MainBannerView, MagazineView
+from .views      import MainBannerView, MagazineView, PicksView
 
 urlpatterns = [
     path('/banner', MainBannerView.as_view()),
     path('/magazine', MagazineView.as_view()),
+    path('/picks', PicksView.as_view()),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -5,68 +5,17 @@ from django.shortcuts import get_object_or_404
 from django.http      import JsonResponse
 from django.db.models import Min, Max
 
-from .models          import Hotel
+from .models          import Hotel, Room
 
 class MainBannerView(View):
     def get(self, request):
-<<<<<<< HEAD
-        try:
-            hotels  = Hotel.objects.all()[:10]
-            banners = [{
-                'index'    : i+1,
-                'hotelName': hotels[i].name,
-                'desc'     : hotels[i].introduction,
-                'image'    : hotels[i].thumbnail_url,
-            } for i in range(len(hotels))]
-            return JsonResponse({'data': banners}, status=200)
-        except ValueError as e:
-            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
-
-class MagazineView(View):
-    def get(self, request):
-        try:
-            hotels    = Hotel.objects.all()[:2]
-            magazines = [{
-                'index'    : i+1,
-                'hotelName': hotels[i].name,
-                'type'     : hotels[i].category.name,
-                'location' : hotels[i].location.name,
-                'descTitle': hotels[i].description_title,
-                'desc'     : hotels[i].description_first,
-                'image'    : hotels[i].thumbnail_url,
-            }  for i in range(len(hotels))]
-            return JsonResponse({'data': magazines}, status=200)
-        except ValueError as e:
-            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
-
-class PicksView(View):
-    def get(self, request):
-        hotels=Hotel.objects.all()
-        picks=[
-            rooms=Room.objects.filter(hotel_id=i)
-            prices=rooms.aggregate(min_p=Min(price_weekday), max_p=Max(price_peak))
-            {
-            'id':hotels[i].id,
-            'name':hotels[i].name,
-            'engName':hotels[i].english_name,
-            'desc':hotels[i].introduction,
-            'mainImg':hotels[i].thumbnail_url,
-            'location':hotels[i].location.name,
-            'type':hotels[i].category.name,
-            'minPrice':prices['min_p'],
-            'maxPrice':prices['max_p'],
-            'stars':list(hotels[i].tags.all())
-            }
-        for i in range(len(hotels))]
-
-        return JsonResponse({'hotels': picks}, status=200)
-=======
         hotels  = Hotel.objects.all()[:10]
         banners = [{
             'hotel_name': hotel.name,
             'desc'      : hotel.introduction,
             'image'     : hotel.thumbnail_url,
         } for hotel in hotels]
+
         return JsonResponse({'data': banners}, status=200)
 
 class MagazineView(View):
@@ -80,6 +29,25 @@ class MagazineView(View):
             'desc'      : hotel.description_first,
             'image'     : hotel.thumbnail_url,
         } for hotel in hotels]
-        return JsonResponse({'data': magazines}, status=200)
->>>>>>> master
 
+        return JsonResponse({'data': magazines}, status=200)
+
+class PicksView(View):
+    def get(self, request):
+        hotels=Hotel.objects.all()
+        picks=[
+            {
+            'id'      : hotel.id,
+            'name'    : hotel.name,
+            'engName' : hotel.english_name,
+            'desc'    : hotel.introduction,
+            'mainImg' : hotel.thumbnail_url,
+            'location': hotel.location.name,
+            'type'    : hotel.category.name,
+            'minPrice': "{:,}".format(int(hotel.room_set.aggregate(min_p=Min('price_weekday'))['min_p'])),
+            'maxPrice': "{:,}".format(int(hotel.room_set.aggregate(max_p=Max('price_peak'))['max_p'])),
+            'stars'   : [tag.name for tag in hotel.tags.all()]
+            }
+        for hotel in hotels]
+
+        return JsonResponse({'hotels': picks}, status=200)
