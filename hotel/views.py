87f4db0d22f465acc033cfb9f8c48269cf68aee0
jek <jihoon.kim89@gmail.com>
2020-10-22 17:00:30 +0900

    Add: added exception handling for view  - 예외처리를 추가했으나 잘 되지 않음

--- hotel/views.py
+++ hotel/views.py
@@ -8,25 +8,31 @@ from .models          import Hotel
 
 class MainBannerView(View):
     def get(self, request):
-        hotels  = Hotel.objects.all()[:10]
-        banners = [{
-            'index'    : i+1,
-            'hotelName': hotels[i].name,
-            'desc'     : hotels[i].introduction,
-            'image'    : hotels[i].thumbnail_url,
-        } for i in range(len(hotels))]
-        return JsonResponse({'data': banners}, status=200)
+        try:
+            hotels  = Hotel.objects.all()[:10]
+            banners = [{
+                'index'    : i+1,
+                'hotelName': hotels[i].name,
+                'desc'     : hotels[i].introduction,
+                'image'    : hotels[i].thumbnail_url,
+            } for i in range(len(hotels))]
+            return JsonResponse({'data': banners}, status=200)
+        except ValueError as e:
+            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
 
 class MagazineView(View):
     def get(self, request):
-        hotels    = Hotel.objects.all()[:2]
-        magazines = [{
-            'index'    : i+1,
-            'hotelName': hotels[i].name,
-            'type'     : hotels[i].category.name,
-            'location' : hotels[i].location.name,
-            'descTitle': hotels[i].description_title,
-            'desc'     : hotels[i].description_first,
-            'image'    : hotels[i].thumbnail_url,
-        }  for i in range(len(hotels))]
-        return JsonResponse({'data': magazines}, status=200)
+        try:
+            hotels    = Hotel.objects.all()[:2]
+            magazines = [{
+                'index'    : i+1,
+                'hotelName': hotels[i].name,
+                'type'     : hotels[i].category.name,
+                'location' : hotels[i].location.name,
+                'descTitle': hotels[i].description_title,
+                'desc'     : hotels[i].description_first,
+                'image'    : hotels[i].thumbnail_url,
+            }  for i in range(len(hotels))]
+            return JsonResponse({'data': magazines}, status=200)
+        except ValueError as e:
+            return JsonResponse({'message': f"{e} IS MISSING"}, status=400)
