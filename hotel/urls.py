d9c93aa3ce27d13ddc61b91997b3a44014368a8f
jek <jihoon.kim89@gmail.com>
2020-10-23 21:47:06 +0900

    add: added detail page API  - 상세페이지를 불러오는 API작성

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views      import MainBannerView, MagazineView, PicksView
+from .views      import MainBannerView, MagazineView, PicksView, DetailPageView
 
 urlpatterns = [
     path('/banner', MainBannerView.as_view()),
     path('/magazine', MagazineView.as_view()),
     path('/picks', PicksView.as_view()),
+    path('/places/<int:hotel_id>', DetailPageView.as_view()),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -50,3 +50,31 @@ class PicksView(View):
         for hotel in hotels]
         
         return JsonResponse({'hotels': picks}, status=200)
+
+class DetailPageView(View):
+    def get(self, request,hotel_id):
+        rooms=Room.objects.select_related('hotel').filter(hotel_id=hotel_id).order_by('id')
+        print('aaa')
+        detail=[
+            {
+              'hotel_name':rooms.first().hotel.name,
+              'hotel_english_name':rooms.first().hotel.english_name,
+              'hotel_introduction':rooms.first().hotel.introduction,
+            },[{
+                'room_name':room.name,
+                'room_type':room.types.get(id=room.id).name,
+                'room_introduction':room.introduction,
+                'checkin_time':room.hotel.checkin_time,
+                'checkout_time':room.hotel.checkout_time,
+                'min_people':room.min_people,
+                'max_people':room.max_people,
+                'area':int(room.area),
+                'bed':[{'bed_type':bed.bed_type.name,'number_of_beds':bed.number}for bed in room.bed_set.all()],
+                'tags':[tag.name for tag in room.hotel.tags.all()] if len(room.hotel.tags.all())<=3 else [tag.name for tag in room.hotel.tags.all()[:3]],
+                'price':chr(0x20A9)+"{:,}".format(int(room.price))+'~',
+                # name과 icon_url을 각각 리스트에 넣을지, 하나의 딕셔너리에 넣을지 해보고 결정
+                'facility':[{'name':facility.name, 'icon_url':facility.icon_url} for facility in room.facilities.all()],
+                'service':[{'name':service.name, 'icon_url':service.icon_url} for service in room.hotel.services.all()]
+            } for room in rooms]
+        ]
+        return JsonResponse({'detail':detail}, status=200)
