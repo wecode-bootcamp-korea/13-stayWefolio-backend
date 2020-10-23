8c75e5636b14d9dd4204828ed748c05a4cdc8a20
jek <jihoon.kim89@gmail.com>
2020-10-23 19:26:28 +0900

    Add: added signup validating decorator, added terms field in user model  - 회원가입 validation용 데코레이터 작성  - user테이블에 사용자 약관 관련 필드 추가

--- user/utils.py
+++ user/utils.py
@@ -1,7 +1,10 @@
+import re
+import json
 import jwt
 
 from django.http import JsonResponse
 
+from user.models import User
 from my_settings import SECRET_KEY, ALGORITHM
 
 def authorize_decorator(func):
@@ -14,4 +17,33 @@ def authorize_decorator(func):
             return JsonResponse({'message': 'INVALID TOKEN'}, status=400)
 
         return func(self, request, *args, **kwargs)
+    return wrapper
+
+def signup_validator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            data             = json.loads(request.body)
+            email            = data['email']
+            name             = data['name']
+            password         = data['password']
+            request.email    = email
+            request.name     = name
+            request.password = password
+            
+            email_pattern    = '^\w+([-_.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$'
+            # 8자 이상, 최소 하나의 문자, 숫자, 특수문자
+            password_pattern = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(email_pattern, email):
+                return JsonResponse({'message': 'INVALID EMAIL'}, status = 400)
+            if not re.match(password_pattern, password):
+                return JsonResponse({'message': 'INVALID PASSWORD'}, status = 400)
+            if User.objects.filter(email = email).exists():
+                return JsonResponse({'message': 'USER ALREADY EXISTS'}, status=400)
+
+            return func(self, request, *args, **kwargs)
+            
+        except KeyError as e:
+            return JsonResponse({'message': f"{e} FIELD IS MISSING"}, status=400)
+
     return wrapper
