bd14d7ce7b1938d8496266bb4a92c983229e65f4
jek <jihoon.kim89@gmail.com>
2020-10-23 11:44:27 +0900

    Add: added picks page view  - Picks 페이지를 불러오는 API 작성  - 프론트와 합의하여 key이름을 DB와 같게 변경

--- hotel/views.py
+++ hotel/views.py
@@ -11,9 +11,9 @@ class MainBannerView(View):
     def get(self, request):
         hotels  = Hotel.objects.all()[:10]
         banners = [{
-            'hotel_name': hotel.name,
-            'desc'      : hotel.introduction,
-            'image'     : hotel.thumbnail_url,
+            'name'         : hotel.name,
+            'introduction' : hotel.introduction,
+            'thumbnail_url': hotel.thumbnail_url,
         } for hotel in hotels]
 
         return JsonResponse({'data': banners}, status=200)
@@ -22,14 +22,13 @@ class MagazineView(View):
     def get(self, request):
         hotels    = Hotel.objects.all()[:2]
         magazines = [{
-            'hotel_name': hotel.name,
-            'type'      : hotel.category.name,
-            'location'  : hotel.location.name,
-            'desc_title': hotel.description_title,
-            'desc'      : hotel.description_first,
-            'image'     : hotel.thumbnail_url,
+            'name'             : hotel.name,
+            'category'         : hotel.category.name,
+            'location'         : hotel.location.name,
+            'description_title': hotel.description_title,
+            'description_first': hotel.description_first,
+            'thumbnail_url'    : hotel.thumbnail_url,
         } for hotel in hotels]
-
         return JsonResponse({'data': magazines}, status=200)
 
 class PicksView(View):
@@ -37,17 +36,17 @@ class PicksView(View):
         hotels=Hotel.objects.all()
         picks=[
             {
-            'id'      : hotel.id,
-            'name'    : hotel.name,
-            'engName' : hotel.english_name,
-            'desc'    : hotel.introduction,
-            'mainImg' : hotel.thumbnail_url,
-            'location': hotel.location.name,
-            'type'    : hotel.category.name,
-            'minPrice': "{:,}".format(int(hotel.room_set.aggregate(min_p=Min('price_weekday'))['min_p'])),
-            'maxPrice': "{:,}".format(int(hotel.room_set.aggregate(max_p=Max('price_peak'))['max_p'])),
-            'stars'   : [tag.name for tag in hotel.tags.all()]
+            'id'           : hotel.id,
+            'name'         : hotel.name,
+            'english_name' : hotel.english_name,
+            'introduction' : hotel.introduction,
+            'thumbnail_url': hotel.thumbnail_url,
+            'location'     : hotel.location.name,
+            'category'     : hotel.category.name,
+            'min_price'    : "{:,}".format(int(hotel.room_set.aggregate(min_p=Min('price_weekday'))['min_p'])),
+            'max_price'    : "{:,}".format(int(hotel.room_set.aggregate(max_p=Max('price_peak'))['max_p'])),
+            'tags'         : [tag.name for tag in hotel.tags.all()]
             }
         for hotel in hotels]
-
+        
         return JsonResponse({'hotels': picks}, status=200)
