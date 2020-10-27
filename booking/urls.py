7c216cf510345c6f4273e0e2eb312cdef4ea0978
jek <jihoon.kim89@gmail.com>
2020-10-27 21:31:52 +0900

    Edit: edited booking view  - 예약정보 저장하는 post 메소드 및 데코레이터 추가

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views      import BookingView
 
 urlpatterns = [
-    path('/new', BookingView.as_view()),
+    path('/<int:room_id>', BookingView.as_view()),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,24 +1,28 @@
 import json
 import datetime
 
-from django.views     import View
-from django.shortcuts import get_object_or_404
-from django.http      import JsonResponse
-from django.db.models import Q
+from django.views           import View
+from django.shortcuts       import get_object_or_404
+from django.http            import JsonResponse
+from django.db.models       import Q
+from django.core.exceptions import ObjectDoesNotExist
 
-from .models          import Booking, BookedRoom
-from user.models      import User
-from hotel.models     import Room
-from user.utils       import authorize_decorator
+from .models                import Booking, BookedRoom
+from user.models            import User
+from hotel.models           import Room
+from user.utils             import authorize_decorator
 
 class BookingView(View):
     @authorize_decorator
-    def get(self, request):
-        room_id           = request.GET.get('room_id')
+    def get(self, request, room_id):
         room              = get_object_or_404(Room, id=room_id)
         year1,month1,day1 = request.GET.get('start').split('-')
         year2,month2,day2 = request.GET.get('end').split('-')
-        bookings          = Booking.objects.filter(Q(date_from__month=month1) | Q(date_from__month=month2), room_id=room_id)
+        bookings          = Booking.objects.filter(
+                                                    Q(date_from__month=month1) | 
+                                                    Q(date_from__month=month2), 
+                                                    room_id=room_id
+                                                )
         date_start        = datetime.date(int(year1), int(month1), int(day1))
         date_end          = datetime.date(int(year2), int(month2), int(day2))
         delta             = datetime.timedelta(days=1)
@@ -32,27 +36,33 @@ class BookingView(View):
         PEAK_MONTH = [1,2,5,6,7,8,12]
         for date in date_list:
             if date.month in PEAK_MONTH:
-                prices[f"{str(date)}"]=room.price_peak
+                prices[f"{str(date)}"]=int(room.price_peak)
             else:
                 if date.weekday()<6:
-                    prices[f"{str(date)}"]=room.price_weekday
+                    prices[f"{str(date)}"]=int(room.price_weekday)
                 else:
-                    prices[f"{str(date)}"]=room.price_weekend
+                    prices[f"{str(date)}"]=int(room.price_weekend)
 
         booking=[{'stay_at_least' : room.stay_at_least,
             'user':{
                 'name' : get_object_or_404(User, id=request.user).name,
                 'email': get_object_or_404(User, id=request.user).email
-            },
-            'booked_dates': [{'date_from':booking.date_from,'date_to':booking.date_to} for booking in bookings],
+                },
+            'booked_dates': [
+                    {
+                        'date_from' : booking.date_from,
+                        'date_to'   : booking.date_to
+                    } for booking in bookings
+                ],
+            'total'       : sum(prices.values()),
             'prices'      : prices
         }]
         return JsonResponse({'booking_info':booking}, status=200)
-
-    def post(self, request):
+    
+    @authorize_decorator
+    def post(self, request, room_id):
         try:
             user_id                       = request.user
-            room_id                       = request.GET.get('room_id')
             data                          = json.loads(request.body)
             date_from                     = data['start']
             date_to                       = data['end']
@@ -102,7 +112,20 @@ class BookingView(View):
                 booking_id = booking.id,
                 room_id    = room_id
             ).save()
-            return JsonResponse({'message':'SUCCESS'}, status=200)
+
+            booking_info=[{
+                'name'      : booking.user.name,
+                'hotel_name': booking.room.hotel.name,
+                'room_name' : booking.room.name,
+                'adult'     : booking.adult,
+                'child'     : booking.child,
+                'infant'    : booking.infant,
+                'date_from' : booking.date_from,
+                'date_to'   : booking.date_to
+            }]
+            return JsonResponse({'booking_info':booking_info}, status=200)
 
         except KeyError as e:
             return JsonResponse({'message':f"{e} IS MISSING"}, status=400)
+        except ObjectDoesNotExist as e:
+            return JsonResponse({'message':f"{e}"}, status=400)
