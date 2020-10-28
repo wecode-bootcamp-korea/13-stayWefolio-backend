4492dd9d46360f42de2bcc9d8a84af7442c19cae
jek <jihoon.kim89@gmail.com>
2020-10-28 17:13:36 +0900

    Edit: edited login view and utils  - jwt의 algorithms->algorithm으로 수정

--- user/utils.py
+++ user/utils.py
@@ -11,7 +11,7 @@ def authorize_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             token=request.headers.get("Authorization")
-            payload=jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+            payload=jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
             request.user=payload['user_id']
         except jwt.exceptions.DecodeError:
             return JsonResponse({'message': 'INVALID TOKEN'}, status=400)
